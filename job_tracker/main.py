f30d58d3878933f2dce7d62da25da2a05410fd60
FeS1111 <ilyamartinov2004@gmail.com>
2026-08-30 16:34:49 +0300

    Final version of CLI application

--- job_tracker/main.py
+++ job_tracker/main.py
@@ -29,6 +29,17 @@ def print_vacancy(vacancy: Vacancy) -> None:
     print(f"Технологии:   {technologies}")
     print("-" * 40)
 
+def print_technology_statistics(technology_statistics: dict[str, int]) -> None:
+    print("\n" + "-" * 40)
+    print("Статистика технологий:")
+    print("-" * 40)
+
+    sorted_statistics = sorted(technology_statistics.items(), key=lambda item: item[1], reverse=True)
+
+    for technology, count in sorted_statistics:
+        print(f"{technology}: {count}")
+    print("-" * 40)
+
 def main() -> None:
     vacancies = load_vacancies()
     tracker = JobTracker(vacancies)
@@ -72,6 +83,55 @@ def main() -> None:
             else:
                 for vacancy in data:
                     print_vacancy(vacancy)
+
+        elif choice == "4":
+            company = input("Введите название компании: ").strip()
+            title = input("Введите название акансии: ").strip()
+
+            print("Доступные статусы:\n", "1. new\n", "2. applied\n", "3. interview\n", "4. rejected")
+            status_input = input("Выберите новый статус: ").strip()
+
+            status_map = {
+                "1": VacancyStatus.NEW,
+                "2": VacancyStatus.APPLIED,
+                "3": VacancyStatus.INTERVIEW,
+                "4": VacancyStatus.REJECTED,
+            }
+
+            new_status = status_map.get(status_input)
+            if new_status is None:
+                print("Введен неверный статус!")
+            else:
+                try:
+                    updated_vacancy = tracker.update_status(company, title, new_status)
+                    save_vacancies(tracker.get_all_vacancies())
+                    print(
+                        f"Статус вакансии '{updated_vacancy.title}' "
+                        f"изменён на '{updated_vacancy.status.value}'"
+                    )
+                except ValueError as error:
+                    print(error)
+
+        elif choice == "5":
+            company = input("Введите название компании: ").strip()
+            title = input("Введите название акансии: ").strip()
+            try:
+                deleted_vacancy = tracker.remove_vacancy(company, title)
+                save_vacancies(tracker.get_all_vacancies())
+                print(
+                    f"Вакансия '{deleted_vacancy.title}' "
+                    f"в компании '{deleted_vacancy.company}' удалена."
+                )
+            except ValueError as error:
+                print(error)
+
+        elif choice == "6":
+            data = tracker.technology_statistics()
+            if not data:
+                print("Список технологий пуст.")
+            else:
+                print_technology_statistics(data)
+
 
         elif choice == "0":
             print("До свидания!")
